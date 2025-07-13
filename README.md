487f3c20f367658cd3c24669606218450cdb12e8
gczcn <494353540@qq.com>
2025-07-14 01:35:42 +0800

    update: 微信跳一跳游戏小程序辅助工具

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ List of all scripts:
 - [move_mouse](https://github.com/gczcn/script/tree/main/move_mouse) : Used to move the cursor position using simple commands
 - [font_downloader](https://github.com/gczcn/script/tree/main/font_downloader) : Used to download some fonts
 - [nvv](https://github.com/gczcn/script/tree/main/nvv) : A very very simple version manager for neovim
-- [consolas_patch](https://github.com/gczcn/script/tree/main/consolas_patch_nerdfont) : Used for patching Consolas fonts
+- [consolas_patcH_nerdfont](https://github.com/gczcn/script/tree/main/consolas_patch_nerdfont) : Used for patching Consolas fonts
+- [tool_of_jump_game](https://github.com/gczcn/script/tree/main/tool_of_jump_game) : Wechat jump game mini program assistant tool (Chinese comments)
---
+++ tool_of_jump_game/main.py
@@ -0,0 +1,63 @@
+# 微信跳一跳小程序辅助工具
+from pynput import mouse
+import random
+import pyautogui
+import time
+
+click_num = 0
+pos_1 = [0, 0]
+pos_2 = [0, 0]
+version = '0.0.1'
+click_status = False
+
+# 关闭 PyAutoGUI 自带的延迟
+pyautogui.PAUSE = 0
+
+# 打印介绍
+print(f'微信跳一跳小程序辅助工具 v{version}')
+
+# 获取系数
+default_coefficient = 7.25
+coefficient = input(f'输入系数（默认系数为 {default_coefficient} ）> ')
+coefficient = default_coefficient if coefficient.strip() == '' else float(coefficient)
+
+# 神秘小提示
+print('提示：请确认微信小程序的窗口和运行该脚本的终端窗口处在同一工作空间，但这并不是必须的，这只是方便您查看信息')
+print('接下来，用鼠标右键分别点击跳一跳起始点和终点，您的鼠标指针将移动到这之间的随机位置并自动长按，待长按结束后重复这一步骤')
+print('按 Ctrl+C 以退出该脚本')
+
+# 开始
+def on_click(x, y, button, pressed):
+    global click_status
+    if button == mouse.Button.right:
+        if pressed:
+            click_status = True
+        else:
+            click_status = False
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+
+while True:
+    if click_status:
+        mouse_pos = pyautogui.position()
+        if click_num == 0:
+            pos_1[0] = mouse_pos[0]
+            pos_1[1] = mouse_pos[1]
+            click_num = 1
+        else:
+            pos_2[0] = mouse_pos[0]
+            pos_2[1] = mouse_pos[1]
+            click_num = 0
+            print(f'起始点: {pos_1}, 终点: {pos_2}')
+            posx = sorted([pos_1[0], pos_2[0]])
+            posy = sorted([pos_1[1], pos_2[1]])
+            click_pos = [random.randint(posx[0], posx[1]), random.randint(posy[0], posy[1])]
+            click_time = coefficient * (abs(pos_2[0] - pos_1[0])) / pyautogui.size()[0]
+            print(f'随机到的长按位置: {click_pos}, 长按时间: {click_time}')
+            pyautogui.moveTo(click_pos[0], click_pos[1], duration=0)
+            pyautogui.mouseDown(button='left')
+            time.sleep(click_time)
+            pyautogui.mouseUp(button='left')
+            print('该步骤完成，继续下一步骤')
+        click_status = False
